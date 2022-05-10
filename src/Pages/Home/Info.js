1591142e44f56b976fa2a0a4797d9a9cb92defe3
Shamim <yeasinshamim7@gmail.com>
2022-05-10 22:23:44 +0600

    info section done

--- src/Pages/Home/Info.js
+++ src/Pages/Home/Info.js
@@ -5,12 +5,17 @@ import marker from '../../assets/icons/marker.svg';
 import phone from '../../assets/icons/phone.svg';
 
 const Info = () => {
+    const infoCardAssets = [
+        { id: 1, img: { clock }.clock, title: "Opening Hours", info: "Lorem Ipsum is simply dummy text of the pri", bgClass: "bg-gradient-to-r from-secondary to-primary" },
+        { id: 2, img: { marker }.marker, title: "Visit our location", info: "Brooklyn, NY 10036, United States", bgClass: "bg-accent" },
+        { id: 3, img: { phone }.phone, title: "Contact us now", info: "+000 123 456789", bgClass: "bg-gradient-to-r from-secondary to-primary" }
+    ];
 
     return (
         <div className='grid grid-cols-1 lg:grid-cols-3 gap-4 px-4 py-8'>
-            <InfoCard key={1} img={clock} />
-            <InfoCard key={2} img={marker} />
-            <InfoCard key={3} img={phone} />
+            {
+                infoCardAssets.map(cardAssets => <InfoCard key={cardAssets.id} img={cardAssets.img} title={cardAssets.title} info={cardAssets.info} bgClass={cardAssets.bgClass} />)
+            }
         </div>
     );
 };
--- src/Pages/Home/InfoCard.js
+++ src/Pages/Home/InfoCard.js
@@ -1,12 +1,12 @@
 import React from 'react';
 
-const InfoCard = ({ img }) => {
+const InfoCard = ({ img, title, info, bgClass }) => {
     return (
-        <div className="card lg:card-side shadow-xl px-6">
+        <div className={`card lg:card-side shadow-xl px-6 ${bgClass}`}>
             <figure><img src={img} alt="Album" /></figure>
             <div className="card-body text-white">
-                <h2 className="card-title">Opening Hours</h2>
-                <p>Click the button to listen on Spotiwhy app.</p>
+                <h2 className="card-title">{title}</h2>
+                <p>{info}</p>
             </div>
         </div>
     );
