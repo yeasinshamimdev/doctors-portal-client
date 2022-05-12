844a25786b0d8c01f52661b4a1181912555a9f98
Shamim <yeasinshamim7@gmail.com>
2022-05-12 19:56:58 +0600

    firebase added

--- src/Pages/Appointment/Service.js
+++ src/Pages/Appointment/Service.js
@@ -5,8 +5,8 @@ const Service = ({ service, setTreatment }) => {
 
     return (
         <div className="card bg-base-100 shadow-xl">
-            <div className="card-body p-0 pt-5 px-4">
-                <h2 className="card-title text-secondary">{name}</h2>
+            <div className="card-body text-center p-10 px-4">
+                <h2 className="text-xl font-bold text-secondary">{name}</h2>
                 <p>
                     {
                         slots.length ? <span>{slots[0]}</span> : <span className='text-red-400'>Try another date</span>
@@ -14,10 +14,10 @@ const Service = ({ service, setTreatment }) => {
                 </p>
 
                 <p>{slots.length} {slots.length > 1 ? 'spaces' : 'space'} </p>
-                <div className="card-actions justify-center mt-4 mb-3">
+                <div className="card-actions justify-center mt-2">
                     <label htmlFor="booking-modal"
                         onClick={() => setTreatment(service)}
-                        disabled={slots.length === 0} className="btn btn-secondary text-white uppercase">book Available</label>
+                        disabled={slots.length === 0} className="btn btn-sm px-4 py-2 shadow-md btn-secondary bg-gradient-to-r from-secondary to-primary text-white uppercase">Book Appointment</label>
                 </div>
             </div>
         </div>
